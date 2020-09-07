13f53db642c09b4017739c19c6e6c00ed579b0f6
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-09-07 11:34:09 +0200

    Finished login window

--- S3_Deadline/Login.xaml.cs
+++ S3_Deadline/Login.xaml.cs
@@ -4,6 +4,7 @@ using S3_Deadline.ViewModels;
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,16 +28,33 @@ namespace S3_Deadline
         {
             InitializeComponent();
             customerViewModel = DataContext as CustomerViewModel;
+            customerViewModel.Initialize();
         }
 
         private void ButtonLogin_Click(object sender, RoutedEventArgs e)
         {
+            Customers selectedCustomer = customerViewModel.SelectedCustomer;
+            string password = textBoxPassword.Text;
             foreach(Customers customer in customerViewModel.Customers)
             {
                 if(customer.CustomerId == textBoxUserName.Text)
                 {
-
+                    selectedCustomer = customer;
                 }
+            }
+            if(selectedCustomer.CustomerId == null)
+            {
+                MessageBox.Show("Forkert brugernavn! prøv igen");
+            }
+            else if(password.ToLower() != "password")
+            {
+                MessageBox.Show("Forkert adgangskode! Prøv igen");
+            }
+            else
+            {
+                Visibility = Visibility.Hidden;
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
             }
         }
     }
