462ece13322ff4ca0bdb58be647ef8a43c3430cd
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-09-07 09:40:13 +0200

    Added login window

---
+++ S3_Deadline/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace S3_Deadline
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login: Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogIn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
